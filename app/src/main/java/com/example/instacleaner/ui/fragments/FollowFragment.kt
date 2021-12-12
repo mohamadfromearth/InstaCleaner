4ded40ba86b1b5f349c02efd98a5b69719f3cf63
Guest1 <mohamadhoseinkhalili174@gmail.com>
2021-12-12 15:13:50 +0330

    Add FollowViewModel.kt

--- app/src/main/java/com/example/instacleaner/ui/fragments/FollowFragment.kt
+++ app/src/main/java/com/example/instacleaner/ui/fragments/FollowFragment.kt
@@ -1,9 +1,28 @@
 package com.example.instacleaner.ui.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DiffUtil
 import com.example.instacleaner.R
+import com.example.instacleaner.databinding.FragmentFollowBinding
+import com.google.android.material.tabs.TabLayout
 
 class FollowFragment:Fragment(R.layout.fragment_follow) {
+
+    private  var _binding:FragmentFollowBinding? = null
+    private val binding:FragmentFollowBinding
+    get() = _binding!!
+
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = DataBindingUtil.bind(view)!!
+    }
 
 
 
---
+++ app/src/main/java/com/example/instacleaner/ui/viewModels/FollowViewModel.kt
@@ -0,0 +1,16 @@
+package com.example.instacleaner.ui.viewModels
+
+import androidx.lifecycle.ViewModel
+import com.example.instacleaner.utils.AccountManager
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class FollowViewModel @Inject constructor(private val accountManager: AccountManager):ViewModel() {
+
+
+
+
+
+
+}
