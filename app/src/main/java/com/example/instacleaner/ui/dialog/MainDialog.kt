4ec2f10d01a5aa5b5db60d5b96f0dfae9b20516e
Guest1 <mohamadhoseinkhalili174@gmail.com>
2021-12-15 10:13:40 +0330

    Add dialog_main.xml

---
+++ app/src/main/java/com/example/instacleaner/ui/dialog/MainDialog.kt
@@ -0,0 +1,11 @@
+package com.example.instacleaner.ui.dialog
+
+import androidx.fragment.app.DialogFragment
+import com.example.instacleaner.R
+
+class MainDialog:DialogFragment(R.layout.dialog_main) {
+
+
+
+
+}
